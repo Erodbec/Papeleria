1e412e01b71ae646b121466dc8b21be03f1a1154
Erodbec <e.rodbec@gmail.com>
2018-04-07 15:42:12 -0500

    Creación
    Conexión Postgresql,, bootstrap

---
+++ includes/conexion.php
@@ -0,0 +1,3 @@
+<?php 
+	$conexion=pg_connect("dbname=Sesiones user=postgres password=system")or die("Lo sentimos pero no se pudo conectar a nuestra db");
+?>
---
+++ views/jumbotron.php
@@ -0,0 +1,33 @@
+
+			<div class="jumbotron">
+						</div>
+
+			 <table class="table table-hover">
+    <thead>
+      <tr>
+        <th>Nombre</th>
+        <th>Email</th>
+        <th>RFC</th>
+        <th>% descuento</th>
+        
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td>Manuel</td>
+        <td>e.rodbec@gmail.com</td>
+        <td>ROBM901122HVZ</td>
+        <td>10</td>
+      </tr>
+      
+    </tbody>
+  </table>
+<div
+<button type="button" class="btn btn-primary">Insertar</button></center><br/>
+</div>
+<div
+<button type="button" class="btn btn-danger">Cancelar</button></center><br/>
+</div>	
+			<div class="col-xs-12 col-sm-12 col-md-4 col-lg-4">
+				
+			</div>
