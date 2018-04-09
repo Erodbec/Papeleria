77bc4429f92fefa7f3cbc023c489064a50a04631
Erodbec <e.rodbec@gmail.com>
2018-04-09 13:29:27 -0500

    sql
    ingresar sql papelería

--- db/base de datos Sesiones.sql
+++
@@ -1,9 +0,0 @@
-﻿Create Table Usuario
-(Id serial Primary key,
-Usuario varchar(15) not null,
-Contrasena varchar(12) not null,
-Email varchar(30) not null,
-TipoUsuario varchar(15) not null
-);
-
-select * from Usuario;
